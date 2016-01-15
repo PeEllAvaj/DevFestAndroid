64eddc200a7dd3ecce34bb268b1c0af016d73ac7
fuentespatrick <fuentespatrick@gmail.com>
2016-01-14 21:09:47 -0600

    Update stubbed method signature and documentation for scheduling the whole conference Stub methods for adding and removing notifications for a single event

--- app/src/main/java/mn/devfest/notifications/LocalNotificationScheduler.java
+++ app/src/main/java/mn/devfest/notifications/LocalNotificationScheduler.java
@@ -1,5 +1,7 @@
 package mn.devfest.notifications;
 
+import android.support.annotation.NonNull;
+
 import java.util.ArrayList;
 
 import mn.devfest.api.model.Session;
@@ -12,10 +14,31 @@ import mn.devfest.api.model.Session;
 public class LocalNotificationScheduler {
 
     /**
-     * Scheduled local notifications that prompt the user to rate a session
-     * @param sessions the sessions to rate
+     * Schedules local notifications that prompt the user to rate all of the sessions in the
+     * conference, if they have notifications turned on at the time of attempted presentation.
+     * This method will remove any existing notifications and replace them with notifications for
+     * the list of sessions it is passed
+     * @param sessions the full list of sessions in the conference
      */
-    public void  scheduleSessionRatingNotifications(ArrayList<Session> sessions) {
+    public void scheduleAllConferenceRatingNotifications(ArrayList<Session> sessions) {
+        //TODO implement
+    }
+
+    /**
+     * Schedules a local notification for rating a single notification. If notifications are turned
+     * off at the time of attempted presentation, the notification won't be presented
+     * @param session the session to ask the user to rate
+     */
+    public void scheduleRatingNotification(@NonNull Session session) {
+        //TODO implement
+    }
+
+    /**
+     * Removes a scheduled local notification for rating a session, provided that a notification has
+     * been scheduled
+     * @param sessionId
+     */
+    public void removeScheduledRatingNotification(@NonNull String sessionId) {
         //TODO implement
     }
 }
