20eb1393e72b7ec57b21824576be001156e19a78
fuentespatrick <fuentespatrick@gmail.com>
2016-01-14 22:06:52 -0600

    Add context & tag to scheduler Document param for removeScheduledRatingNotification() Flesh out scheduleRatingNotification() Add TODO to add ID to notification tag mapper

--- app/src/main/java/mn/devfest/notifications/LocalNotificationScheduler.java
+++ app/src/main/java/mn/devfest/notifications/LocalNotificationScheduler.java
@@ -1,5 +1,8 @@
 package mn.devfest.notifications;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import java.util.ArrayList;
@@ -12,6 +15,15 @@ import mn.devfest.api.model.Session;
  * @author pfuentes
  */
 public class LocalNotificationScheduler {
+
+    public static final String SESSION_RATING_EXTRA = "mn.devfest.SessionForRating";
+
+    Context mContext;
+
+    //TODO move to dependency injection
+    public LocalNotificationScheduler(@NonNull Context context) {
+        mContext = context;
+    }
 
     /**
      * Schedules local notifications that prompt the user to rate all of the sessions in the
@@ -30,13 +42,16 @@ public class LocalNotificationScheduler {
      * @param session the session to ask the user to rate
      */
     public void scheduleRatingNotification(@NonNull Session session) {
-        //TODO implement
+        Intent intent = new Intent(mContext, LocalNotificationPresentationService.class);
+        intent.putExtra(SESSION_RATING_EXTRA, session);
+        //TODO replace '42' with a mapper from event IDs to notification tags
+        PendingIntent pendingIntent = PendingIntent.getService(mContext, 42, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     /**
      * Removes a scheduled local notification for rating a session, provided that a notification has
      * been scheduled
-     * @param sessionId
+     * @param sessionId id of the session to remove the existing notification for
      */
     public void removeScheduledRatingNotification(@NonNull String sessionId) {
         //TODO implement
