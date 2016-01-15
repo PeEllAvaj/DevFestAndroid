7feb4601279922476f187c5dbb4a934f7e782c00
fuentespatrick <fuentespatrick@gmail.com>
2016-01-14 20:47:34 -0600

    Stub a scheduler class Stub a method for scheduling notifications

---
+++ app/src/main/java/mn/devfest/notifications/LocalNotificationScheduler.java
@@ -0,0 +1,21 @@
+package mn.devfest.notifications;
+
+import java.util.ArrayList;
+
+import mn.devfest.api.model.Session;
+
+/**
+ * Manages scheduling local notifications
+ *
+ * @author pfuentes
+ */
+public class LocalNotificationScheduler {
+
+    /**
+     * Scheduled local notifications that prompt the user to rate a session
+     * @param sessions the sessions to rate
+     */
+    public void  scheduleSessionRatingNotifications(ArrayList<Session> sessions) {
+        //TODO implement
+    }
+}
